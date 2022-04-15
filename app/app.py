a9d6e933746c791d199fcf0e90e5558560f409c4
Felipe Operti <felipe.operti@intervieweb.it>
2022-04-15 21:35:39 +0200

    update

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import fitz
 
 st.title('Frontiers Journal Recommendation')
 
@@ -8,7 +9,29 @@ upload_insert = st.selectbox("Would you prefer to upload a pdf paper or insert a
             ["UPLOAD","INSERT"], 
             index=1)
 
+text = None
+
 if upload_insert == "INSERT":
-    st.text_area("Insert here the text of the paper", value="")
+    text = st.text_area("Insert here the text of the paper", value="")
 else:
-    st.file_uploader("Upload here the pdf of the paper", type="pdf", accept_multiple_files=False)
+    uploaded_file = st.file_uploader("Upload here the pdf of the paper", 
+                     type="pdf", 
+                     accept_multiple_files=False)
+    if uploaded_file:
+        with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:
+            text = ""
+            for page in doc:
+                text += page.get_text()
+
+model = st.selectbox(
+     "Select the model",
+     ('SBERT', 'TFIDF', 'Word2Vec'))
+
+embedding_type = st.radio("Select the embedding type", 
+                                ["All document","Only keywords"], 
+                                index=1)     
+
+
+
+
+
